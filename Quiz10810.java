c4b010e3577e962e6bea2330a28aa33cb9db704e
ganghagyeong <gkrrod714@naver.com>
2024-05-13 20:52:56 +0900

    20240513

---
+++ Quiz10810.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Quiz10810 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            for (int j = a-1; j < b; j++) {
+                arr[j] = c;
+            }
+
+        }
+        for (int i = 0; i < N; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        br.close();
+    }
+}
+
---
+++ Quiz10818.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Quiz10818 {
+    public static void main(String[] args) throws IOException {
+      Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] a = new int[N];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int max = a[0];
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+
+        int min = a[0];
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < min) {
+                min = a[i];
+            }
+        }
+
+        System.out.println(min + " " + max);
+        sc.close();
+    }
+
+}
